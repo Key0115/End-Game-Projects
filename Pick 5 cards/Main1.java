e43ebbbefaec182690a480745cc4d16940e18a1e
Key0115 <95165731+Key0115@users.noreply.github.com>
2023-03-20 15:42:39 +0000

    Update Main1.java

--- Pick 5 cards/Main1.java
+++ Pick 5 cards/Main1.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-    public class Main1 {
+    public class Main {
         public static void main(String[] args) {
             Deck myDeck = new Deck();//Make a new deck
             myDeck.cardCreater();
